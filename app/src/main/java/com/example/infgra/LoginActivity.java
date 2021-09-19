04ecc5a494c9413bbf1620a656456e4135f80a1a
santergameboy <santergameboy@gmail.com>
2021-09-19 18:56:23 -0400

    TextView Clickable

--- app/src/main/java/com/example/infgra/LoginActivity.java
+++ app/src/main/java/com/example/infgra/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.infgra;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void goCreateAccount(View view){
+
+        System.out.println("-----------------> go create Account");
+
     }
 }
