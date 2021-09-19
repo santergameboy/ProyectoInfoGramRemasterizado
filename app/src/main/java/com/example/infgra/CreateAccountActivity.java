f944ba95dca670809c7b47a6ff44b16d322366dd
santergameboy <santergameboy@gmail.com>
2021-09-19 19:58:53 -0400

    ToolBar

--- app/src/main/java/com/example/infgra/CreateAccountActivity.java
+++ app/src/main/java/com/example/infgra/CreateAccountActivity.java
@@ -1,6 +1,7 @@
 package com.example.infgra;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,15 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        showToolBar(getString(R.string.title_crear_cuenta),true);
+    }
+    public void showToolBar(String titulo,boolean botonsubir){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonsubir);
+
     }
 }
